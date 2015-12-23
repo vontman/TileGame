f832eaf1087cbd0a908a8ae9d0d781146473377f
vontman <vontman@gmail.com>
2015-12-23 03:19:57 +0200

    Fixed an image for each light to increase performance Move the declaration of pixels array from invert alpha method to the constructor

--- TileGame/src/myTileGame/gfx/Light.java
+++ TileGame/src/myTileGame/gfx/Light.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.RadialGradientPaint;
-import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
 import myTileGame.objects.entites.Entity;
@@ -14,11 +13,41 @@ public class Light {
 	private float power;
 	private float x,y;
 	private Entity entity;
+	private BufferedImage img;
 	public Light(float x,float y,float radius,float power) {
 		this.x = x;
 		this.y = y;
 		this.radius = radius;
 		this.power = Math.min(1F, power);
+		
+		
+		img = new BufferedImage((int)radius*2,(int)radius*2,BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g = (Graphics2D) img.getGraphics();
+		RadialGradientPaint gp = new RadialGradientPaint(new Point(
+	    		(int)(radius),(int)(radius))
+	    		,radius,new float[]{.0F,power},
+	    		new Color[]{new Color(0,0,0,255),new Color(0,0,0,3)});
+		g.setPaint(gp);
+		g.fillRect(0,0, (int)radius*2, (int)radius*2);
+	    		
+	}
+	public BufferedImage getImage(){
+		return img;
+	}
+	public float getRadius() {
+		return radius;
+	}
+	public float getPower() {
+		return power;
+	}
+	public float getX() {
+		return x-radius;
+	}
+	public float getY() {
+		return y-radius;
+	}
+	public BufferedImage getImg() {
+		return img;
 	}
 	public void focusOnEntity(Entity e){
 		entity = e;
@@ -38,4 +67,5 @@ public class Light {
 		g.fillRect((int)(x-xOffset-radius),(int)(y-yOffset-radius), (int)radius*2, (int)radius*2);
 	    		
 	}
+	
 }
--- TileGame/src/myTileGame/gfx/LightMap.java
+++ TileGame/src/myTileGame/gfx/LightMap.java
@@ -2,8 +2,6 @@ package myTileGame.gfx;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.Point;
-import java.awt.RadialGradientPaint;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.util.ArrayList;
@@ -15,10 +13,13 @@ import myTileGame.entites.objects.creatures.Player;
 public class LightMap {
 	private BufferedImage lightMap;
 	private List<Light>lights;
-	Handler handler;
+	private Handler handler;
+	private int[]pixels;
 	public LightMap(Handler handler,Player player) {
 		this.handler = handler;
 		lightMap = new BufferedImage(handler.getGameWidth(),handler.getGameHeight(),BufferedImage.TYPE_INT_ARGB);
+		pixels = ((DataBufferInt)lightMap.getRaster().getDataBuffer()).getData();
+		
 		lights = new ArrayList<Light>();
 		Light light = new Light(0,0,200,.9F);
 		light.focusOnEntity(player);
@@ -36,7 +37,6 @@ public class LightMap {
 		}
 	}
 	private void invertAlpha(){
-		int pixels[] = ((DataBufferInt)lightMap.getRaster().getDataBuffer()).getData();
 		for(int i=0;i<pixels.length;i++){
 			Color color = new Color(pixels[i],true);
 			pixels[i] = new Color(
@@ -52,7 +52,8 @@ public class LightMap {
 		g2.setBackground(new Color(0,0,0,0));
 		g2.clearRect(0, 0, handler.getGameWidth(), handler.getGameHeight());
 		for(Light l : lights){
-			l.render(g2, xOffset, yOffset);
+//			l.render(g2, xOffset, yOffset);
+			g2.drawImage(l.getImage(), (int)(l.getX()-xOffset), (int)(l.getY()-yOffset), null);
 		}
 		invertAlpha();
 		g2.dispose();
